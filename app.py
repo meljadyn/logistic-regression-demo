136c65287c097e3dec839020c4c7ebcb279139a5
Melody Duncan <melody.d.duncan@gmail.com>
2023-10-26 19:46:58 -0400

    Add requirements file

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score
